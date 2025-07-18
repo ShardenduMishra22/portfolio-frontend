809f90d46f8a74655189b844ccad8b4d33b97ba1
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-07-19 04:29:04 +0530

    refactor: clean up layout component and remove unused metadata

--- src/app/blog/landing/page.tsx
+++ src/app/blog/landing/page.tsx
@@ -1,7 +1,9 @@
+import React from 'react'
+
 const page = () => {
   return (
     <div>
-      hellow world
+      
     </div>
   )
 }
--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,34 +1,17 @@
 "use client"
-
 import { authClient } from "@/lib/authClient"
 import { useRouter } from "next/navigation"
-import { ReactNode, useEffect } from "react"
+import { JSX, ReactNode, useEffect } from "react"
 
-export const metadata = {
-  title: "Blog | Mishra Shardendu",
-  description: "Explore insights and thoughts by Mishra Shardendu on tech, development, and more.",
-  openGraph: {
-    title: "Blog | Mishra Shardendu",
-    description: "Explore insights and thoughts by Mishra Shardendu on tech, development, and more.",
-    url: "https://mishrashardendu22.is-a.dev/blog",
-    type: "website",
-    siteName: "Shardendu Mishra Portfolio",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Blog | Mishra Shardendu",
-    description: "Explore insights and thoughts by Mishra Shardendu on tech, development, and more.",
-  },
-}
-
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }): JSX.Element {
   const router = useRouter()
-
   useEffect(() => {
     const checkSession = async () => {
       const session = await authClient.getSession()
       if (session) {
         router.push("/blog/landing")
+      }else {
+        router.push("/blog")
       }
     }
     checkSession()
