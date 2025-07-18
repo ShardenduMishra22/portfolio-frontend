fb5df6d136857d512e99cd8d8defa1bf79ea77a7
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-07-18 22:50:22 +0530

    Fix HTML entity for apostrophe in blog introduction paragraph

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ const page = () => {
   return (
     <div>
       <h1>Blog</h1>
-      <p>Welcome to my blog! Here you'll find my latest thoughts and updates.</p>
+      <p>Welcome to my blog! Here you&apos;ll find my latest thoughts and updates.</p>
     </div>
   )
 }
