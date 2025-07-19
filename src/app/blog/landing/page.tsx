74a63a0640c9c78beacb596ec930656c73715509
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-07-19 16:21:55 +0530

    feat: implement user session handling in blog landing page

--- src/app/blog/landing/page.tsx
+++ src/app/blog/landing/page.tsx
@@ -1,9 +1,16 @@
+import { authClient } from '@/lib/authClient'
 import React from 'react'
 
 const page = () => {
+  const session = authClient.useSession();
+
   return (
     <div>
-      
+      {session ? (
+        <p>Welcome back, {session.data?.user.name}!</p>
+      ) : (
+        <p>Please log in to access your blog.</p>
+      )}
     </div>
   )
 }
