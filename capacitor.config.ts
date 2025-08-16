23ef43d035960476dc4f9a0c934f1e6b9d722d8f
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-08-16 16:16:10 +0530

    testing
    Signed-off-by: MishraShardendu22 <shardendumishrabrave@gmail.com>

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.isadev.mishrashardendu22',
+  appName: 'Mishra Portfolio',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
